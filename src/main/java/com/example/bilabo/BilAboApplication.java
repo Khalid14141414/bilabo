11a2245bb233b3bf5bcb5f33043a2347397ef14c
khalid behi <khalidfarah86@gmail.com>
2024-05-07 14:49:19 +0200

    Initial commit

---
+++ src/main/java/com/example/bilabo/BilAboApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilabo/BilAboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
